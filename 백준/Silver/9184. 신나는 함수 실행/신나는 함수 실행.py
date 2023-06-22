f31c1d970dfc3a9012dd8ebbdf7a24226859aefb
nembizzang <105214855+nembizzang@users.noreply.github.com>
2023-06-22 19:59:51 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 68 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+def w(a,b,c):
+    if a <= 0 or b <= 0 or c <= 0:
+        return 1
+    if a > 20 or b > 20 or c > 20:
+        return w(20,20,20)
+    if dp[a][b][c]:
+        return dp[a][b][c]
+    if a<b<c:
+        dp[a][b][c] = w(a, b, c-1) + w(a, b-1, c-1) - w(a, b-1, c)
+        return dp[a][b][c]
+    dp[a][b][c] = w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) - w(a-1, b-1, c-1)
+    return dp[a][b][c]
+
+# 0~20 까지 이므로 21개로 만들어줌
+dp = [[[0]*21 for _ in range(21)] for _ in range(21)]
+
+while True:
+    a,b,c = map(int,input().split())
+    if a==b==c==-1:
+        break
+    print('w({}, {}, {}) = {}'.format(a, b, c, w(a,b,c)))
