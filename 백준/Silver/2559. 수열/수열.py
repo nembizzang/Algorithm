d3e51f0186b606e3cd85c0cd8a9c3f1a8afceabe
nembizzang <105214855+nembizzang@users.noreply.github.com>
2023-07-26 02:01:57 +0900

    [Silver III] Title: 수열, Time: 108 ms, Memory: 40180 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.py
+++ 백준/Silver/2559. 수열/수열.py
@@ -5,12 +5,15 @@ n,k = map(int,input().split())
 temps = list(map(int,input().split()))
 # 시간복잡도를 줄이기 위해 stack 사용
 left, right = deque(temps[:k]), deque(temps[k:])
+
 # 1일~k일까지 온도의 합
 ans = k_sum = sum(left)
+
+# right에 남은 숫자가 없을 때까지 right의 왼쪽에서 숫자 하나씩 빼줄거임
 while right :
-    new_num = right.popleft()
-    left.append(new_num)
-    k_sum += new_num - left.popleft()
+    new_num = right.popleft() # right에서 왼쪽에서 숫자 빼서
+    left.append(new_num) # left의 오른쪽에 넣어줌
+    k_sum += new_num - left.popleft() # 현재 합을 변경
     if ans < k_sum:
-        ans = k_sum
+        ans = k_sum # 그 합이 ans보다 크면 ans 최신화
 print(ans)
