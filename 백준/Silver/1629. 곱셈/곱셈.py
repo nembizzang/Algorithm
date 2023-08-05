ca563a44dcde21dc1c771536e279d26856a7fc88
nembizzang <105214855+nembizzang@users.noreply.github.com>
2023-08-05 17:30:55 +0900

    [Silver I] Title: 곱셈, Time: 56 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1629. 곱셈/곱셈.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+a,b,c = map(int,input().split())
+rests = [] # a**i의 나머지 집합으로 0은 a**0의 나머지
+i = 1
+rests.append(a%c)
+# rests에는 a**1의 나머지, a**2의 나머지, a**4의 나머지, a**8의 나머지...가 들어간다.
+while i <= b:
+    rests.append((rests[-1]**2)%c)
+    i *= 2
+ans = 1
+for rest,i in zip(rests,bin(b)[2:][::-1]): # b의 2진법을 뒤집었다.
+    if int(i):
+        ans *= (rest*int(i))%c
+print(ans%c)
