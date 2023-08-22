e30b6b5a46e77572004703e672b0cb97aa6b10f0
nembizzang <105214855+nembizzang@users.noreply.github.com>
2023-08-22 16:48:03 +0900

    [Silver I] Title: 미로 탐색, Time: 72 ms, Memory: 34176 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -9,13 +9,12 @@ def bfs(row,col,cnt):
     stack.append([row,col,cnt])
     while stack :
         row,col,cnt = stack.popleft()
-        if (row==n-1)&(col==m-1): # 최초로 도착했다면
+        if (row==n-1)&(col==m-1): # 최초로 마지막 칸에 도착했다면
             return(cnt)
-        for d_row,d_col in [[-1,0],[1,0],[0,-1],[0,1]] : # 상하좌우이동
+        for d_row,d_col in [[-1,0],[1,0],[0,-1],[0,1]] : # 상하좌우 이동으로 도착할 다음 칸
             n_row, n_col = row+d_row, col+d_col # 이동하려는 칸 초기화
-            if (0<=n_row<n) & (0<=n_col<m) : # 새로 이동하는 칸이 maze 내 범위이면
-                tmp = maze[n_row][n_col]
-                if (tmp == 1) or (tmp > cnt): # 처음 가거나 재방문이지만 더 짧은 이동시간에 도착하는거라면
+            if (0<=n_row<n) & (0<=n_col<m) : # 새로 이동하는 칸이 maze 내 범위이고
+                if maze[n_row][n_col] == 1 : # 이번 칸이 처음 이동하는 칸이라면
                     stack.append([n_row,n_col,cnt+1]) # 이동하는 칸에서 또 확인
-                    maze[n_row][n_col] = cnt
-print(bfs(0,0,1))
+                    maze[n_row][n_col] = cnt+1 # 다음번 도착하는 칸 초기화
+print(bfs(0,0,1)) # 이번 행, 이번 열, 이번 칸 도착 시 밟은 칸 수
