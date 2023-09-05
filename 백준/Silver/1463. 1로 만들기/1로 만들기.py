77eb1166b3cc5293674517922b346cfa666ef4a5
nembizzang <105214855+nembizzang@users.noreply.github.com>
2023-09-06 08:30:59 +0900

    [Silver III] Title: 1로 만들기, Time: 68 ms, Memory: 34120 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -9,15 +9,15 @@ def bfs(n):
         if i==1:
             return cnt
         cnt += 1
-        if (not i%3) and (i>=3) and (not dp[i//3]):
+        if (not i%3)and (not visited[i//3]):
             stack.append([i//3,cnt])
-            dp[i%3] = 1
-        if (not i%2) and (i>=2) and (not dp[i//2]):
+            visited[i%3] = 1
+        if (not i%2)and (not visited[i//2]):
             stack.append([i//2,cnt])
-            dp[i%2] = 1
-        if (not dp[i-1]):
+            visited[i%2] = 1
+        if (not visited[i-1]):
             stack.append([i-1,cnt])
-            dp[i-1] = 1
+            visited[i-1] = 1
 n = int(input())
-dp = defaultdict(int)
+visited = defaultdict(int)
 print(bfs(n))
