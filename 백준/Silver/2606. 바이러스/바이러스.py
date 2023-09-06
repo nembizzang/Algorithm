7991faaace43794744f5f29d8805484099e2adc0
nembizzang <105214855+nembizzang@users.noreply.github.com>
2023-09-06 21:28:21 +0900

    [Silver III] Title: 바이러스, Time: 60 ms, Memory: 34140 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,25 +1,22 @@
 import sys
+from collections import deque, defaultdict
 input = sys.stdin.readline
+def bfs(i):
+    stack = deque([i])
+    while stack:
+        for j in dic[stack.popleft()]:
+            if not visited[j] : # 방문한 적이 없다면
+                visited[j]=1 # 방문표시
+                stack.append(j)
+    return sum(visited.values())
 
 n = int(input())
-t = int(input())
-
-dic = {}
-
-for i in range(n) :
-  dic[i+1] = []
-
-for i in range(t) :
-  a,b = map(int, input().split())
-  dic[a].append(b)
-  dic[b].append(a)
-
-def dfs(dic, start) :
-  for i in dic[start] :
-    if i not in visited:
-      visited.append(i)
-      dfs(dic,i)
-
-visited = [1]
-dfs(dic,1)
-print(len(visited)-1)
+dic = defaultdict(list)
+for _ in range(int(input())):
+    a,b = map(int,input().split())
+    dic[a].append(b)
+    dic[b].append(a)
+    
+visited = {i:0 for i in range(1,n+1)}
+visited[1] = 1
+print(bfs(1)-1)
