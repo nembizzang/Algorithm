cd6fb547181819a1b81b146097313b91cf014d62
김영빈 <105214855+nembizzang@users.noreply.github.com>
2023-09-12 03:48:49 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 44 ms, Memory: 33456 KB -BaekjoonHub

--- 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.py
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.py
@@ -1,19 +1,21 @@
 import sys
-from collections import deque
+from heapq import heappush, heappop
 input = sys.stdin.readline
 
 def bfs():
-    stack = deque([[1,0]]) # 출발점은 1번이고, 0회 이동
-    while stack :
-        tmp, cnt = stack.popleft() # 현 위치, 이동 횟수
+    heap = [[0,1]] # 0회 이동, 출발점은 1번
+    while heap :
+        cnt, tmp = heappop(heap) # 현 위치, 이동 횟수
         cnt += 1 # 이동 횟수 증가
         for i in range(1,7): # 주사위 굴리기
             if tmp+i <= 100 : # 이동 가능하다면
                 new = mov[tmp+i] # 새로운 곳으로 이동
-                if visited[new]>cnt : # 현재 기록된 최소 방문 횟수보다 작다면
+                if visited[new ] >cnt : # 현재 기록된 최소 방문 횟수보다 작다면
                     visited[new] = cnt # 최소 방문 횟수 초기화
+                    if new == 100 : # 도착 했다면
+                        return cnt
                     if new != 100 : # 도착한 것이 아니라면
-                        stack.append([new,cnt]) # 새롭게 이동
+                        heappush(heap,[cnt,new]) # 새롭게 이동
     return visited[100]
 n,m = map(int,input().split())
 mov,visited = {},{} # 이동하는 칸(뱀,사다리), 해당 칸 최소 방문 횟수
