0fdeeb28f5a3bbc72fbf1c74d3c8f867f54d6494
김영빈 <105214855+nembizzang@users.noreply.github.com>
2023-09-12 22:38:03 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 2144 ms, Memory: 117832 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
@@ -1,46 +1,34 @@
-import sys
-from heapq import heappop
-input = sys.stdin.readline
+from collections import deque
 
-def bfs():
-    heap = [[1,0,0,0]]
-    while heap :
-        cnt,y,x,crashed = heappop(heap)
-        if y==n-1 and x==m-1 : # 도착했다면 반환
-            return cnt # 도착 칸 수
-        cnt += 1
-        if 0<y and not visited[y-1][x][crashed]: # 이동 가능하고 최초 방문이라면
-            if maps[y-1][x] == 1 and not crashed :# 벽이고 부순 적이 없다면
-                visited[y-1][x][1] = 1 # 벽 부수고 방문
-                heap.append([cnt,y-1,x,1]) # 이동칸수,y,x,crashed heap에 넣기
-            elif not maps[y-1][x] : # 벽이 아니라면
-                visited[y-1][x][crashed] = 1 # 벽 안부수고 방문
-                heap.append([cnt,y-1,x,crashed]) # 이동칸수,y,x,crashed heap에 넣기
-        if y<n-1 and not visited[y+1][x][crashed] : # 이동 가능하고 최초 방문이라면
-            if maps[y+1][x] == 1 and not crashed :# 벽이고 부순 적이 없다면
-                visited[y+1][x][1] = 1 # 벽 부수고 방문
-                heap.append([cnt,y+1,x,1]) # 이동칸수,y,x,crashed heap에 넣기
-            elif not maps[y+1][x] : # 벽이 아니라면
-                visited[y+1][x][crashed] = 1 # 벽 안부수고 방문
-                heap.append([cnt,y+1,x,crashed]) # 이동칸수,y,x,crashed heap에 넣기
-        if 0<x and not visited[y][x-1][crashed] : # 이동 가능하고 최초 방문이라면
-            if maps[y][x-1] == 1 and not crashed :# 벽이고 부순 적이 없다면
-                visited[y][x-1][1] = 1 # 벽 부수고 방문
-                heap.append([cnt,y,x-1,1]) # 이동칸수,y,x,crashed heap에 넣기
-            elif not maps[y][x-1] : # 벽이 아니라면
-                visited[y][x-1][crashed] = 1 # 벽 안부수고 방문
-                heap.append([cnt,y,x-1,crashed]) # 이동칸수,y,x,crashed heap에 넣기
-        if x<m-1 and not visited[y][x+1][crashed] : # 이동 가능하고 최초 방문이라면
-            if maps[y][x+1] == 1 and not crashed :# 벽이고 부순 적이 없다면
-                visited[y][x+1][1] = 1 # 벽 부수고 방문
-                heap.append([cnt,y,x+1,1]) # 이동칸수,y,x,crashed heap에 넣기
-            elif not maps[y][x+1] : # 벽이 아니라면
-                visited[y][x+1][crashed] = 1 # 벽 안부수고 방문
-                heap.append([cnt,y,x+1,crashed]) # 이동칸수,y,x,crashed heap에 넣기
+def shortest_path(N, M, matrix):
+    # Initialize distances array with -1
+    distances = [[[-1 for _ in range(M)] for _ in range(N)] for _ in range(2)]
+    # Initialize queue for BFS
+    queue = deque()
+    # Add starting point to queue
+    queue.append((0, 0, 0))
+    # Set distance of starting point to 1
+    distances[0][0][0] = 1
+    # Define directions for movement
+    directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    
+    while queue:
+        x, y, z = queue.popleft()
+        if x == N-1 and y == M-1:
+            return distances[z][x][y]
+        for dx, dy in directions:
+            nx = x + dx
+            ny = y + dy
+            if nx >= 0 and nx < N and ny >= 0 and ny < M:
+                if matrix[nx][ny] == '0' and distances[z][nx][ny] == -1:
+                    distances[z][nx][ny] = distances[z][x][y] + 1
+                    queue.append((nx, ny, z))
+                elif z == 0 and matrix[nx][ny] == '1' and distances[1][nx][ny] == -1:
+                    distances[1][nx][ny] = distances[z][x][y] + 1
+                    queue.append((nx, ny, 1))
     return -1
 
-n,m = map(int,input().split())
-maps = [list(map(int,list(input().strip()))) for _ in range(n)]
-visited = [[[0]*2 for _ in range(m)] for _ in range(n)] # [y][x][0]은 안뚫 방문 [y][x][1]은 벽뚫방문
-visited[0][0][0]=1 # 첫번째 칸 방문 이력 추가
-print(bfs())
+# Example test case
+N, M = map(int, input().split())
+matrix = [list(input()) for _ in range(N)]
+print(shortest_path(N, M, matrix)) # Output: 15
