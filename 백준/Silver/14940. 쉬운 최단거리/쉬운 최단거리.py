1ebc57bba075069cca0714ce85dade29a8b84293
김영빈 <105214855+nembizzang@users.noreply.github.com>
2023-09-14 03:30:25 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 504 ms, Memory: 39752 KB -BaekjoonHub

---
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
@@ -0,0 +1,33 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+def bfs(y,x):
+    stack = deque([[y,x]])
+    while stack :
+        y,x = stack.popleft()
+        for i in range(4):
+            ny,nx = y+dy[i],x+dx[i]
+            if 0<=ny<n and 0<=nx<m and maps[ny][nx] and visited[ny][nx]>visited[y][x]+1:
+                visited[ny][nx] = visited[y][x]+1
+                stack.append([ny,nx])
+n,m = map(int,input().split())
+maps = []
+goal = []
+for i in range(n):
+    row = list(map(int,input().split()))
+    maps.append(row)
+    try : goal += [i,row.index(2)]
+    except : continue
+    # if 2 in row :
+    #    goal += [i,row.index(2)]
+visited = [[n*m]*m for _ in range(n)]
+visited[goal[0]][goal[1]]=0
+dy,dx = [-1,1,0,0],[0,0,-1,1]
+bfs(*goal)
+for i in range(n):
+    row = visited[i]
+    for j,num in enumerate(row):
+        if num == n*m : # 가지 못한 곳
+            row[j] = 0 if not maps[i][j] else -1 # 벽이면 0 아니면 -1   
+    print(*row)
