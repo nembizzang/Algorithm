b2392e4a6fd4d62861adfd50f509a3176c9bd977
김영빈 <105214855+nembizzang@users.noreply.github.com>
2024-01-18 17:04:45 +0900

    [level 3] Title: 야근 지수, Time: 490.73 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.py
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.py
@@ -10,16 +10,14 @@ def solution(n, works):
     while n and len(works)>=2 : # 야근 시간이 남아있고 작업 종류가 2개 이상이라면
         a = heappop(works)
         b = works[0]
-        if n >= b-a+1: # 야근 시간이 최대값을 차대값-1까지 만들만큼 충분하다면
-            n -= b-a+1 # 야근하기
-            a = b+1
-        else :
-            a += n
-            n = 0
+        if n < b-a+1: # 야근 시간이 최대값을 차대값-1까지 만들만큼 충분하지 않다면
+            heappush(works,a+n) # a를 n만큼 일하고 heappush
+            return sum(map(lambda x : x**2,works)) # 모든 일을 다했으므로
+        n -= b-a+1 # 야근하기
+        a = b+1
+        if a: # a가 작업량이 남았다면(b가 1짜리 일인 경우)
             heappush(works,a)
-            return sum(map(lambda x : x**2,works))
-        if a: # a가 작업량이 남았다면
-            heappush(works,a)
+
     if n == 0 : # 작업 종류가 2개 이상인데 야근 시간이 없는 경우
         return sum(map(lambda x : x**2,works))
     # 야근 시간은 있는데 작업 종류가 1개인 경우
