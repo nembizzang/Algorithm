dbbcbd395c9baf25d92153c9ebc2745653c9075d
김영빈 <105214855+nembizzang@users.noreply.github.com>
2024-01-22 00:48:50 +0900

    [level 3] Title: 최고의 집합, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/12938. 최고의 집합/최고의 집합.py
+++ 프로그래머스/3/12938. 최고의 집합/최고의 집합.py
@@ -1,7 +1,6 @@
 '''
-1) n==1 이면 return [s]
-2) s를 n으로 나눈 몫(t), 나머지(r)을 구하여 t가 s-r개, t+1이 r개 있는 list를 return
-3) 몫이 0이면 return [-1]
+1) s를 n으로 나눈 몫(t), 나머지(r)을 구하여 t가 s-r개, t+1이 r개 있는 list를 return
+2) 몫이 0이면 return [-1]
 '''
 def solution(n, s):
     t,r = divmod(s,n)
